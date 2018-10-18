852c80afebb33bead97cda00fc741f44a16e025e
michelet <michelet@usp.br>
2018-10-18 18:20:24 -0300

    dockerized

--- app/app.py
+++ app/app.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from flask_cors import CORS
+from flask_restful import Api
+
+from lista.resources.item import Item,ItemList
 
 app = Flask(__name__)
+api = Api(app)
+CORS(app,resources={r"/*": {"origins": "*"}}) #O uso do cors
+api.add_resource(ItemList, '/itens')
+api.add_resource(Item, '/item','/item/<string:item>')
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!!'
+if __name__ == '__main__':
+    app.run(debug=True)
