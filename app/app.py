386eb5db1e6cb2e2b1122125dd439d3c6178eda1
michelet <michelet@usp.br>
2018-10-19 16:31:22 -0300

    refatoração do api v2

--- app/app.py
+++ app/app.py
@@ -1,9 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_restful import Api
-
 from lista.resources.api_v1 import Item,ItemList
-
 app = Flask(__name__)
 api = Api(app)
 CORS(app,resources={r"/*": {"origins": "*"}}) #O uso do cors
